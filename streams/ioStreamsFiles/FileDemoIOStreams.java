201fcb14e95f71d1b18f9b1850f349aa4c053e78
Pavithra N S <110277400+Paviii-Shetty@users.noreply.github.com>
2025-06-16 11:15:11 +0530

    Add files via upload

---
+++ streams/ioStreamsFiles/FileDemoIOStreams.java
@@ -0,0 +1,37 @@
+package streams.ioStreamsFiles;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class FileDemoIOStreams {
+	public static void main(String[] args) throws IOException {
+		FileReader reader = new FileReader(new File(
+				"C:\\Users\\ps924291.ttl\\OneDrive - TATA MOTORS LTD\\Desktop\\javaFeaturesSTS\\Java8Features\\src\\streams\\ioStreamsFiles\\demo.txt"));
+		BufferedReader br = new BufferedReader(reader);
+		String line = br.readLine();
+		while (line != null) {
+			System.out.println(line);
+			line = br.readLine();
+		}
+		br.close();
+		System.out.println("=====================================");
+		String filename = "C:\\Users\\ps924291.ttl\\OneDrive - TATA MOTORS LTD\\Desktop\\javaFeaturesSTS\\Java8Features\\src\\streams\\ioStreamsFiles\\demo.txt";
+
+		List<String> lines = new ArrayList<>();
+		try (Stream<String> streamLine = Files.lines(Paths.get(filename))) {
+			streamLine.filter(linesH -> linesH.startsWith("H")||linesH.startsWith("h"))
+			.forEach(fileLine -> System.out.println(fileLine));
+		} catch (Exception e) {
+			e.printStackTrace();// TODO: handle exception
+		}
+
+	}
+
+}
